e2f205fb8880743dcbc511ce3a74af02ce2df06c
Genilson <geniilsonfernandes@gmail.com>
2022-04-23 11:31:09 -0300

    create var colors

--- src/components/PostCardBig/index.jsx
+++ src/components/PostCardBig/index.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 export const PostCardBig = () => {
-  return <div>PostCardBig</div>;
+  return (
+    <div className={styles.box}>
+      PostCardBig
+      <ul className={styles.boxes}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul className={styles.text}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 };
