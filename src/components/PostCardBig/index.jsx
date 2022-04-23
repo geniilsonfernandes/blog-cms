5a79f2cbd1814dc2f755ebef1ed6b4f291cd29e9
Genilson <geniilsonfernandes@gmail.com>
2022-04-23 13:25:48 -0300

    create component postcardbig

--- src/components/PostCardBig/index.jsx
+++ src/components/PostCardBig/index.jsx
@@ -1,30 +1,34 @@
+import Image from "next/image";
 import React from "react";
 import styles from "./styles.module.scss";
 
-export const PostCardBig = () => {
+export const PostCardBig = ({ imageURL = false }) => {
   return (
-    <div className={styles.box}>
-      PostCardBig
-      <ul className={styles.boxes}>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-      <ul className={styles.text}>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+    <div className={styles.wrapper__post}>
+      {imageURL && <div className={styles.post__photo}></div>}
+
+      <div className={styles.post__details}>
+        <span className={styles.tag}>CODE, GAMING</span>
+        <h2 className={styles.title}>
+          From its medieval origins to the digital era, learn everything there
+          is to know
+        </h2>
+        <div className={styles.metadata}>
+          <div className={styles.user}>
+            <span>Genilson fernandes</span>
+          </div>
+
+          <div className={styles.date}>
+            <span>May, 17, 2022</span>
+          </div>
+        </div>
+        <p className={styles.excerpet}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut...
+        </p>
+        <button className={styles.button}> Read More </button>
+      </div>
     </div>
   );
 };
